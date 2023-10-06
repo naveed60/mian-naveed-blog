608991cb4dd2ffdf37a6a59a26e7006dd98290d6
Naveed <advmiannaveedali@gmail.com>
2023-10-06 18:47:45 +0500

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,6 @@ Rails.application.routes.draw do
   get 'contact_us' => 'pages#contact_us'
   get 'privacy_policy' => 'pages#privacy_policy'
   root 'home#index'
-end
- 
-# config/routes.rb
-
-Rails.application.routes.draw do
-  root "articles#index"
 
   resources :articles
-  get 'show' => 'articles#show'
 end
-
