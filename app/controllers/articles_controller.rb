7c6ac74269afed0098574a328b09978ff15f1df9
Naveed <advmiannaveedali@gmail.com>
2023-10-09 18:28:06 +0500

    article controller destroy method updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to root_path, status: :see_other
+    redirect_to @article, notice: 'Record was successfully destroyed.'
   end
 
   private
